5e74470adbfb2fe661222de33d033908f265a65c
Fernando Monterroso <fmonterroso.tpp@gmail.com>
2013-05-08 18:44:20 -0600

    Creacion de aplicacion y paginas listado y configurar solo interfaz grafica phonegap

---
+++ src/com/consumodedatos/Principal.java
@@ -0,0 +1,18 @@
+package com.consumodedatos;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import org.apache.cordova.*; 
+
+public class Principal extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		//setContentView(R.layout.activity_principal);
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html"); //archivo de ventana principal (listado)
+	}
+
+
+}
